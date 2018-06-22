d681293dd2f6541f0bee11315c22350a29bc71e2
Sudheer Penubarthy <always.sudheer@gmail.com>
2018-06-22 00:28:55 -0700

    Removing secutity token

--- cloneIt.sh
+++ cloneIt.sh
@@ -10,7 +10,7 @@ git clone --bare https://github.com/$oldName/$oldRepo.git
 cd $oldRepo.git
 git lfs fetch --all
 echo 'now to create a repo in' $newName
-curl -H "Authorization: token 13c6b077327e7d2db0ade0f2b44b5164a863f771" https://api.github.com/user/repos -d "{\"name\":\"$oldRepo\"}"
+curl -H "Authorization: token <Add git oauth token here>" https://api.github.com/user/repos -d "{\"name\":\"$oldRepo\"}"
 git push --mirror https://github.com/$newName/$oldRepo.git
 git lfs push --all https://github.com/$newName/$oldRepo.git
 cd ..
